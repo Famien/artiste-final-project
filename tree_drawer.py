013d89a442941b065df33f4cc910623ef0fd5c2d
Yang Dai <ydai144@gmail.com>
2018-05-14 11:17:04 -0400

    add final parse trees

--- tree_drawer.py
+++ tree_drawer.py
@@ -1,12 +1,37 @@
-from nltk import Tree
-from nltk.draw.util import CanvasFrame
-from nltk.draw import TreeWidget
-import os
-
-cf = CanvasFrame()
-t = Tree.fromstring('(S (NP the Dodgers) (VP (VP (V beat) (NP the Red Sox)) (CCVP (CC and) (VP (VBD were) (VBN beaten) (PP (P by) (NP the Giants))))))')
-tc = TreeWidget(cf.canvas(),t)
-cf.add_widget(tc,10,10) # (10,10) offsets
+##from nltk import Tree
+##from nltk.draw.util import CanvasFrame
+##from nltk.draw import TreeWidget
+##import os
+##
+##cf = CanvasFrame()
+##tstr = '(VP (V have wanted) (barVP (barVPword to) (VP (V know) (PP (P for) (NP (N many years))))))'
+##t = Tree.fromstring(tstr)
+##tc = TreeWidget(cf.canvas(),t)
+##cf.add_widget(tc,10,10) # (10,10) offsets
 ##cf.print_to_file('tree.ps')
 ##cf.destroy()
 ##os.system('convert tree.ps tree.png')
+
+def main():
+    from nltk import Tree
+    from nltk.draw.util import CanvasFrame
+    from nltk.draw import TreeWidget
+    import os
+    import sys
+
+    file = sys.argv[1]
+    f = open(file,"r")
+    i = 1
+    for s in f.readlines():
+        cf = CanvasFrame()
+        t = Tree.fromstring(s)
+        tc = TreeWidget(cf.canvas(),t)
+        cf.add_widget(tc,10,10) # (10,10) offsets
+        nf = "trees/tree"+str(i)+".ps"
+        cf.print_to_file(nf)
+        cf.destroy()
+        #os.system("mv "+nf+" trees/"+nf)
+        print(i)
+        i+=1
+
+main()	
